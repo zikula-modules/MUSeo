df33cb205ed8cb8b0dfd634b3b4a2d6eb8d1e089
Guite <info@guite.de>
2014-11-27 17:15:11 +0100

    simplified method logic

--- lib/MUSeo/Listener/ModuleDispatch.php
+++ lib/MUSeo/Listener/ModuleDispatch.php
@@ -50,56 +50,51 @@ class MUSeo_Listener_ModuleDispatch
         // we get the module args for the event
         $modargs = $event->getArgs();
 
-        if ($modargs['modname'] != 'Blocks' && $modargs['modname'] != 'Admin') {
-
-            // we look if a module is active for MUSeo
-            $modules = MUSeo_Api_HandleModules::checkModules();
-
-            // if we found a module we can do more
-            if (is_array($modules) && count($modules) > 0) {
-
-                // we get the allowed controllers
-                $controllers = ModUtil::getVar('MUSeo', 'controllers');
-                $controllers = explode(',', $controllers);
-                if (in_array($modargs['modfunc'][1], $controllers)) {
-                    //LogUtil::registerStatus($modargs['modfunc'][1]);
-
-                    if ($modargs['type'] == 'admin') {
-                        // admin call nothing to do
-                    } else {
-                        //LogUtil::registerStatus('User');
-                        // we look for not api functions
-                        if ($modargs['api'] != 1) {
-                            //LogUtil::registerStatus('keine Api');
-
-                            // we look if there is an entry for the module with the relevant name and func
-                            $metatagrepository = MUSeo_Util_Model::getMetatagRepository();
-                            $where = 'tbl.theModule = \'' . DataUtil::formatForStore($modargs['modname']) . '\'';
-                            $where .= ' AND ';
-                            $where .= 'tbl.functionOfModule = \'' . DataUtil::formatForStore($modargs['modfunc'][1]) . '\'';
-                            $count = $metatagrepository->selectCount($where);
-
-                            if ($count >= 1) {
-                                // we call the method to set the metatags if there is an entry
-                                MUSeo_Api_Handlemodules::setModuleMetaTags($modargs['modname'], $modargs['modfunc'][1]);
-                            }
-                        }
-                        // api function - nothing to do
-                        else {
-                            //LogUtil::registerStatus('Api');
-                        }
-                    }
-                }
-                //not allowed controller - nothing to do
-                else {
-                }
-            }
-            // no active modules, nothing to do
-            else {
-            }
+        if (in_array($modargs['modname'], array('Blocks', 'Admin'))) {
+            // nothing to do for module blocks
+            return;
         }
-        // calling module blocks nothing to
-        else {
+
+        if ($modargs['type'] == 'admin') {
+            // admin call, thus nothing to do
+            return;
+        }
+
+        // check if MUSeo is activated for any modules
+        $modules = MUSeo_Api_HandleModules::checkModules();
+        if (!is_array($modules) || count($modules) < 1) {
+            // no active modules, thus nothing to do
+            return;
+        }
+
+        // get list of allowed controllers
+        $controllers = ModUtil::getVar('MUSeo', 'controllers');
+        $controllers = explode(',', $controllers);
+
+        if (!in_array($modargs['modfunc'][1], $controllers)) {
+            // unallowed controller, thus nothing to do
+            return;
+        }
+
+        //LogUtil::registerStatus($modargs['modfunc'][1]);
+
+        // we are not interested in api functions
+        if ($modargs['api'] == 1) {
+            return;
+        }
+
+        // now check if an entry for the module exists with corresponding name and func
+        $metatagRepository = MUSeo_Util_Model::getMetatagRepository();
+        $where = 'tbl.theModule = \'' . DataUtil::formatForStore($modargs['modname']) . '\'';
+        $where .= ' AND ';
+        $where .= 'tbl.functionOfModule = \'' . DataUtil::formatForStore($modargs['modfunc'][1]) . '\'';
+        $count = $metatagRepository->selectCount($where);
+
+        if ($count >= 1) {
+            // there is an entry
+
+            // set the metatags accordingly
+            MUSeo_Api_Handlemodules::setModuleMetaTags($modargs['modname'], $modargs['modfunc'][1]);
         }
     }
 
